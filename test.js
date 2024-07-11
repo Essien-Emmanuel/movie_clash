ed4d8224b0eb54a4e82c5ca12a70a92fde63cd2b
Essien Emmanuel <essienemma300@gmail.com>
2024-07-11 13:39:40 +0100

    added mocha test html file

--- test.js
+++
@@ -1,7 +0,0 @@
-//update key dynamically
-const updateObjectKey = (obj, oldkey, newKey) => {
-  const descriptor = Object.getOwnPropertyDescriptor(obj, oldkey);
-  Object.defineProperty(obj, newKey, descriptor);
-  delete obj[oldkey];
-  return obj;
-}
