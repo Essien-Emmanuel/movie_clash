1c3225e303892d5a279b2b1a94d939795add9dbf
Essien Emmanuel <essienemma300@gmail.com>
2024-07-11 18:12:03 +0100

    added test for dropdown section of the widget

--- project_copy_for_test/test/autocomplete.test.js
+++ project_copy_for_test/test/autocomplete.test.js
@@ -1,4 +1,5 @@
-it("Shows autocomplete widget", () => {
+beforeEach(() => {
+  document.querySelector("#target").innerHTML = "";
   createAutoComplete({
     root: document.querySelector("#target"),
 
@@ -14,4 +15,22 @@ it("Shows autocomplete widget", () => {
       return movie.Title;
     },
   });
+});
+
+it("Dropdown starts closed", () => {
+  const dropdown = document.querySelector(".dropdown");
+
+  if (dropdown.className === "is-active")
+    throw new Error("Expected dropdown to not be active");
+});
+
+it("After searching, dropdown opens again", () => {
+  const input = document.querySelector("input");
+  input.innerText = "Avengers";
+  input.dispatchEvent(new Event("input"));
+
+  const dropdown = document.querySelector(".dropdown");
+
+  if (dropdown.className === "is-active")
+    throw new Error("Expected dropdown to not be active");
 });
